d17b860f9257c4869e8c2a3ad106df5ac3c0816a
Alex <alexfilsecker@gmail.com>
2024-07-12 13:16:04 +1000

    reload activated

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+import os
 
 from scrapers.santander import scrap_santander
 from scrapers.commonwealth import scrap_commonwealth
+
+in_container = os.environ.get("IN_CONTAINER", "false") == "true"
 
 app = FastAPI()
 
@@ -24,14 +27,16 @@ async def hello_world():
 
 @app.post("/santander")
 async def post_santander(credentials: SantanderCredentials):
-    movements = scrap_santander(credentials.rut, credentials.password, headless=False)
+    movements = scrap_santander(
+        credentials.rut, credentials.password, headless=in_container
+    )
     return movements
 
 
 @app.post("/common-wealth")
 async def post_common_wealth(credentials: CommonWealthCredentials):
     total_pending, pending_movements, non_pending_movements = scrap_commonwealth(
-        credentials.client_number, credentials.password, headless=True
+        credentials.client_number, credentials.password, headless=in_container
     )
 
     return {
@@ -41,3 +46,14 @@ async def post_common_wealth(credentials: CommonWealthCredentials):
         },
         "non_pending": non_pending_movements,
     }
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    if in_container:
+        port = 8080
+    else:
+        port = 8000
+
+    uvicorn.run(app="main:app", host="0.0.0.0", port=port, reload=True)
